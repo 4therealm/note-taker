67f3aa32a1cc57e9a435ca7b8a6b92a2e551c999
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-30 14:32:55 -0600

    idk

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //page navigation
 app.use(express.static("public"));
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 //catch all
 app.get("*", (req, res) =>res.send("/public/404.html"));
 
